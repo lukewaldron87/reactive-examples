1031e71169a4300dd5b0d3c5a09a30e9b5267238
Luke Waldron <lukewaldron87@gmail.com>
2023-08-04 11:16:28 +0200

    added Person and Repository

---
+++ src/main/java/com/waldron/springframework/reactiveexamples/PersonRepository.java
@@ -0,0 +1,12 @@
+package com.waldron.springframework.reactiveexamples;
+
+import com.waldron.springframework.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface PersonRepository {
+
+    Mono<Person> getById(Integer id);
+
+    Flux<Person> findAll();
+}
---
+++ src/main/java/com/waldron/springframework/reactiveexamples/PersonRepositoryImpl.java
@@ -0,0 +1,17 @@
+package com.waldron.springframework.reactiveexamples;
+
+import com.waldron.springframework.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class PersonRepositoryImpl implements PersonRepository {
+    @Override
+    public Mono<Person> getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Flux<Person> findAll() {
+        return null;
+    }
+}
