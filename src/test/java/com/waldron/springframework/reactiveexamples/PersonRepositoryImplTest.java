3929c411c668e5e29cf8927203684622fd181cd4
Luke Waldron <lukewaldron87@gmail.com>
2023-08-04 11:47:04 +0200

    testing flux

--- src/test/java/com/waldron/springframework/reactiveexamples/PersonRepositoryImplTest.java
+++ src/test/java/com/waldron/springframework/reactiveexamples/PersonRepositoryImplTest.java
@@ -3,7 +3,10 @@ package com.waldron.springframework.reactiveexamples;
 import com.waldron.springframework.reactiveexamples.domain.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -43,6 +46,37 @@ class PersonRepositoryImplTest {
             return person.getFirstName();
         }).subscribe(firstName -> {
             System.out.println("from map: "+firstName);
+        });
+    }
+
+    @Test
+    void fluxTestBlockFirst() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Person person = personFlux.blockFirst();
+
+        System.out.println(person.toString());
+    }
+
+    @Test
+    void testFluxSubscribe() {
+        Flux<Person> personFlux = personRepository.findAll();
+
+        personFlux.subscribe(person -> {
+            System.out.println(person.toString());
+        });
+    }
+
+    @Test
+    void testFluxToListMono(){
+        Flux<Person> personFlux = personRepository.findAll();
+
+        Mono<List<Person>> personListMono = personFlux.collectList();
+
+        personListMono.subscribe(list -> {
+            list.forEach(person -> {
+                System.out.println(person.toString());
+            });
         });
     }
 }
